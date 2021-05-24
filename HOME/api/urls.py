2499c3e8aa8032abba019c49756b1adda088ab9f
Anshuman265 <72643139+Anshuman265@users.noreply.github.com>
2021-05-24 15:39:08 +0530

    Backend Django

---
+++ HOME/api/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from .views import UserRecordView
+
+app_name = 'api'
+urlpatterns = [
+    path('user/', UserRecordView.as_view(), name='users'),
+]
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.authtoken import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls', namespace='api')),
+    path('api-token-auth/', views.obtain_auth_token, name='api-token-auth'),
+]
